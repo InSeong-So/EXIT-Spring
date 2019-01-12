23c69a72fd57e4922efc3c2fbee61d5a384a0150
InSeong-So <goflvhxj2547@gmail.com>
2019-01-12 10:04:21 +0900

    add spring study code

---
+++ src/main/java/step1/MainClass.java
@@ -0,0 +1,11 @@
+package step1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		TransportationWalk transportationwork = new TransportationWalk();
+		transportationwork.move();
+
+	}
+}
