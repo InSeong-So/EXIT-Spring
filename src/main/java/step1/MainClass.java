276bb2be8f86e98a0f264cb6d7f763c7e35669d7
InSeong-So <goflvhxj2547@gmail.com>
2019-01-12 10:20:04 +0900

    applicationContext 추가, 스프링 컨테이너 사용, 개념 이해

--- src/main/java/step1/MainClass.java
+++ src/main/java/step1/MainClass.java
@@ -1,11 +1,26 @@
 package step1;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class MainClass {
 
 	public static void main(String[] args) {
+		// 스프링 컨테이너 사용 전
+		// TransportationWalk transportationwork = new TransportationWalk();
+		// transportationwork.move();
 
-		TransportationWalk transportationwork = new TransportationWalk();
-		transportationwork.move();
+		// 스프링 컨테이너 사용
+		// 생성한 컨테이너에 접근하기 위한 객체 생성
+		// GenericApplicationContext 하위의 GenericXmlApplicationContext
+		GenericXmlApplicationContext context = new GenericXmlApplicationContext("classpath:applicationContext.xml");
 
+		// 스프링 컨테이너에서 생성한 빈 객체를 가져오고 변수로 선언하기
+		TransportationWalk transportationWalk = context.getBean("walk", TransportationWalk.class);
+
+		// 가져온 빈 객체 사용하기
+		transportationWalk.move();
+
+		// 사용한 빈 객체 반환
+		context.close();
 	}
 }
